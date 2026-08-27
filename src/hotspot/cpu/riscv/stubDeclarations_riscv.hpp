7142501139508a0e2f3fc196aa3f380fe5230105
Kuai Wei <kuaiwei.kw@alibaba-inc.com>
2026-08-27 17:02:03 +0800

    draft patch of string indexOf intrinsic

--- src/hotspot/cpu/riscv/stubDeclarations_riscv.hpp
+++ src/hotspot/cpu/riscv/stubDeclarations_riscv.hpp
@@ -55,7 +55,7 @@
                                        do_arch_entry,                   \
                                        do_arch_entry_init,              \
                                        do_arch_entry_array)             \
-  do_arch_blob(compiler, 45000)                                         \
+  do_arch_blob(compiler, 48000)                                         \
   do_stub(compiler, compare_long_string_LL)                             \
   do_arch_entry(riscv, compiler, compare_long_string_LL,                \
                 compare_long_string_LL, compare_long_string_LL)         \
@@ -77,6 +77,15 @@
   do_stub(compiler, string_indexof_linear_ul)                           \
   do_arch_entry(riscv, compiler, string_indexof_linear_ul,              \
                 string_indexof_linear_ul, string_indexof_linear_ul)     \
+  do_stub(compiler, string_indexof_linear_ll_v)                         \
+  do_arch_entry(riscv, compiler, string_indexof_linear_ll_v,            \
+                string_indexof_linear_ll_v, string_indexof_linear_ll_v) \
+  do_stub(compiler, string_indexof_linear_uu_v)                         \
+  do_arch_entry(riscv, compiler, string_indexof_linear_uu_v,            \
+                string_indexof_linear_uu_v, string_indexof_linear_uu_v) \
+  do_stub(compiler, string_indexof_linear_ul_v)                         \
+  do_arch_entry(riscv, compiler, string_indexof_linear_ul_v,            \
+                string_indexof_linear_ul_v, string_indexof_linear_ul_v) \
   do_stub(compiler, arrays_hashcode_powers_of_31)                       \
   do_arch_entry(riscv, compiler, arrays_hashcode_powers_of_31,          \
             arrays_hashcode_powers_of_31, arrays_hashcode_powers_of_31) \
